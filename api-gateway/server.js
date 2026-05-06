c4a482021b6182750640804223ec07222e35eeb1
utilswindows-svg <utilswindows@gmail.com>
2026-05-06 15:34:21 +0530

    Initial commit

--- api-gateway/server.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-const app = require('./app');
-
-const PORT = Number(process.env.GATEWAY_PORT) || 3001;
-
-app.listen(PORT, () => {
-  console.log(`🚀 API Gateway listening on http://localhost:${PORT}`);
-  console.log(`📡 Backend service: ${process.env.BACKEND_URL}`);
-});
